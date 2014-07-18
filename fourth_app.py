7a929a5e1e066bcff76323acd46165260b82fe4c
phillipsKP <kjphillips@suffolk.edu>
2014-07-17 20:47:12 -0400

    test fourth app

--- fourth_app.py
+++ fourth_app.py
@@ -33,3 +33,17 @@ def page_not_found(error):
 
 app.debug = True
 app.run()
+
+###
+
+
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World! zzzzzzzzzzzzzz'
+
+###

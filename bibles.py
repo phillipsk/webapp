1f8581d9bb3726bac0971987c658e79b10589d08
phillipsKP <kjphillips@suffolk.edu>
2014-08-07 20:39:45 -0400

    helloflask dir made & heroku doc followed

---
+++ bibles.py
@@ -0,0 +1,6 @@
+key = 'fs46TaQgYcukC15upxE4Sp7ej9Rvz7B0fgJO0P0U'
+
+#bp = curl -u fs46TaQgYcukC15upxE4Sp7ej9Rvz7B0fgJO0P0U:X -k https://bibles.org/v2/versions/eng-GNTD.js
+bp = "curl -u str(key):X -k https://bibles.org/v2/versions/eng-GNTD.js"
+
+print bp
---
+++ david.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import scriptures
+scriptures.extract('This is a test Rom 3:23-28 and 1 JOHn 2')
+[('Romans', 3, 23, 3, 28), ('I John', 2, 1, 2, 29)]

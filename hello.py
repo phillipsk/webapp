240edd650f0ae460bd9a18e45d3f2088385ce3f0
phillipsKP <kjphillips@suffolk.edu>
2014-08-07 20:48:56 -0400

    test

--- hello.py
+++ hello.py
@@ -1,3 +1,18 @@
+
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World! zzzzzzzzzzzzzz'
+
+#<div id="hi_btn"   class=btn btn-info">Say Hi</div>
+app.run()
+
+
+"""
 import flask, flask.views
 import os
 import functools
@@ -68,3 +83,4 @@ app.add_url_rule('/music/',
 
 app.debug = True
 app.run()
+"""
